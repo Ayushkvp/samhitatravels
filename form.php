ceb4e78bd1c12631602ed66224d0cb1ad05a277e
ayushkp <96825046+Ayushkvp@users.noreply.github.com>
2022-01-29 21:51:23 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Exo+2:wght@200&family=Pacifico&display=swap");
+      body {
+        /* font-family: "Pacifico", cursive; */
+        font-family: "Exo 2", sans-serif;
+        background-image: linear-gradient(
+          to bottom right,
+          rgb(10, 240, 133),
+          rgb(70, 206, 224),
+          rgb(3, 112, 185)
+        );
+        background-repeat: no-repeat;
+        height: 100vh;
+        justify-content: center;
+      }
+
+      h1 {
+        justify-content: center;
+        display: flex;
+        text-align: center;
+      }
+
+      a {
+        text-decoration: none;
+        color: black;
+        font-size: 20px;
+        display: flex;
+        text-align: center;
+        width: 20%;
+        margin: 10% 40%;
+        box-sizing: border-box;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Thanks for submitting the form</h1>
+    <a href="home.html">Return to home page</a>
+  </body>
+</html>
